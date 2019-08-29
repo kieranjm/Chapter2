41ff00981ea4cae43e425209fea5a36a199d1974
0787461 <0787461@KELCE-LABS29-P.pittstate.edu>
2019-08-29 11:12:06 -0500

    1st commit

---
+++ src/main/java/com/mycompany/chapter2/Oof.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author 0787461
+ */
+public class Oof {
+    
+}
